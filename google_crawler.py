ea1fd7583e980ee36c03265a78566ae51435b3f7
Hugo Sousa <hsousa@gmx.com>
2018-10-07 22:26:43 -0300

    google_crawler: add automatic fact checking for 4 fact checkers

--- google_crawler.py
+++ google_crawler.py
@@ -20,7 +20,7 @@ import urllib.request
 FACT_CHECKERS = ['boatos.org', 'e-farsas.com', 'g1.globo.com/e-ou-nao-e',
                  'piaui.folha.uol.com.br/lupa', 'g1.globo.com/fato-ou-fake'
                  'veja.abril.com.br/blog/me-engana-que-eu-posto',
-                 'aosfatos.org', ]
+                 'aosfatos.org']
 
 TIME_PARAM = '%2Ccdr%3A1%2Ccd_min%3A1%2F1%2F0%2Ccd_max%3A&tbm='
 URL = 'http://images.google.com.br/searchbyimage?image_url=' + \
@@ -34,6 +34,10 @@ MONTHS = {y: (x + 1) for (x, y) in enumerate(['jan', 'fev', 'mar',
                                               'abr', 'mai', 'jun',
                                               'jul', 'ago', 'set',
                                               'out', 'nov', 'dez'])}
+
+headers = [('User-Agent', USER_AGENT)]
+opener = urllib.request.build_opener()
+opener.addheaders = headers
 
 
 def process_url(url):
@@ -64,25 +68,32 @@ def get_html(url, sleep_min, sleep_max, redirect=False):
     '''
 
     url = process_url(url)
-    headers = [('User-Agent', USER_AGENT)]
-    opener = urllib.request.build_opener()
-    opener.addheaders = headers
 
     if redirect:
-        with closing(opener.open(url)) as open_url:
-            redirect_url = open_url.url
-            new_url = ''.join(redirect_url.split('&')[:-1]) + TIME_PARAM
-            url = process_url(new_url)
+        while True:
+            try:
+                with closing(opener.open(url)) as open_url:
+                    redirect_url = open_url.url
+                    new_url = ''.join(redirect_url.split('&')
+                                      [:-1]) + TIME_PARAM
+                    url = process_url(new_url)
+                    slow_down(sleep_min, sleep_max)
+                    break
+            except Exception:
+                print('\t\t[-] Exception occurred, retrying.')
+                continue
+
+    while True:
+        try:
+            with closing(opener.open(url)) as open_url:
+                html = open_url.read().decode()
+                slow_down(sleep_min, sleep_max)
+                break
+        except urllib.error.HTTPError as http_error:
+            print(http_error, '\tURL:', url)
             slow_down(sleep_min, sleep_max)
+            continue
 
-    try:
-        with closing(opener.open(url)) as open_url:
-            html = open_url.read().decode()
-    except urllib.error.HTTPError as http_error:
-        print(http_error, '\tURL:', url)
-        exit()
-
-    slow_down(sleep_min, sleep_max)
     return html
 
 
@@ -110,11 +121,7 @@ def parse_date(raw_date):
         return ''
 
 
-a = 1
-
-
 def get_page_sources(html):
-    global a
     '''
         Get image sources on a particular page.
 
@@ -251,3 +258,170 @@ def is_fact_checked(sources):
 
     links = set([s[0] for s in sources])
     return any(f in l for f in FACT_CHECKERS for l in links)
+
+
+def check_boatos(link, sleep_min, sleep_max):
+    '''
+        Check boatos.org judgment about a content.
+
+        @link: (string) Link to content.
+        @sleep_min: (float) Minimum amount of seconds to sleep for.
+        @sleep_max: (float) Maximum amount of seconds to sleep for.
+
+        @return: (bool) True iff the content was considered true.
+    '''
+
+    # Check if link is root.
+    split = link.split('//')
+    if len(split) >= 2:
+        split = split[1].split('/')
+
+        if len(split) >= 2:
+            if split[1] == '':
+                return None
+
+    # In case it's not root
+    html = get_html(link, sleep_min, sleep_max)
+    return '#boato' in html
+
+
+def check_efarsas(link, sleep_min, sleep_max):
+    '''
+        Check e-farsas judgment about a content.
+
+        @link: (string) Link to content.
+        @sleep_min: (float) Minimum amount of seconds to sleep for.
+        @sleep_max: (float) Maximum amount of seconds to sleep for.
+
+        @return: (bool) True iff the content was considered true.
+    '''
+
+    # Check if link is root or blog.
+    split = link.split('//')
+    if len(split) >= 2:
+        split = split[1].split('/')
+
+        if len(split) >= 2:
+            if split[1] == '' or split[1] == 'blog':
+                return None
+
+    html = get_html(link, sleep_min, sleep_max)
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find_all('span', {'class': 'mvp-post-cat left'})
+
+    if len(title) > 0:
+        tag = title[0].string.lower()
+        return tag == 'falso'
+
+    # tags = [a.string for a in soup.find_all('a', {'rel': 'tag'})]
+    return None
+
+
+def check_e_ou_nao_e(link, sleep_min, sleep_max):
+    '''
+        Check G1 É ou não É judgment about a content.
+
+        @link: (string) Link to content.
+        @sleep_min: (float) Minimum amount of seconds to sleep for.
+        @sleep_max: (float) Maximum amount of seconds to sleep for.
+
+        @return: (bool) True iff the content was considered true.
+    '''
+
+    # Check if link is root.
+    split = link.split('//')
+    if len(split) >= 2:
+        split = split[1].split('/')
+
+        if len(split) <= 2:
+            return None
+
+    html = get_html(link, sleep_min, sleep_max)
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find_all('h1', {'class': 'content-head__title'})
+
+    if len(title) > 0:
+        title = title[0].string.lower()
+        return 'É verdade!' in title
+
+    return None
+
+
+def check_lupa(link, sleep_min, sleep_max):
+    '''
+        Check Lupa judgment about a content.
+
+        @link: (string) Link to content.
+        @sleep_min: (float) Minimum amount of seconds to sleep for.
+        @sleep_max: (float) Maximum amount of seconds to sleep for.
+
+        @return: (bool) True iff the content was considered true.
+    '''
+
+    # Check if link is root.
+    split = link.split('//')
+    if len(split) >= 2:
+        split = split[1].split('/')
+
+        if len(split) <= 2:
+            return None
+
+    html = get_html(link, sleep_min, sleep_max)
+    soup = BeautifulSoup(html, 'html.parser')
+    post = soup.find_all('div', {'class': 'post-inner'})
+
+    if len(post) > 0:
+        tag_false = post.find_all('div', {'class': 'etiqueta etiqueta-7'})
+        tag_true = post.find_all(
+            'div', {'class': ['etiqueta etiqueta-1', 'etiqueta etiqueta-2']})
+
+        if len(tag_false) > 0:
+            return False
+
+        if len(tag_true) > 0:
+            return True
+
+    return None
+
+
+def get_fact_check(sources, sleep_min, sleep_max):
+    '''
+        Check if a particular image was fact checked true or false.
+
+        @sources: ((string, string) list) List of sources where the image has
+            appeared.
+        @sleep_min: (float) Minimum amount of seconds to sleep for.
+        @sleep_max: (float) Maximum amount of seconds to sleep for.
+
+        @return: (dict) Dictionary with the fact checkers and their fact check
+            judgment.
+    '''
+
+    fact_checks = {f: None for f in FACT_CHECKERS}
+
+    for source, source_date in sources:
+        if source_date == '':
+            continue
+
+        if any(f in source for f in FACT_CHECKERS):  # Fact checker link
+            f = 'boatos.org'  # Boatos
+            if f in source and fact_checks[f] is not True:
+                fact_checks[f] = check_boatos(source, sleep_min, sleep_max)
+                continue
+
+            f = 'e-farsas.com'  # E-Farsas
+            if f in source and fact_checks[f] is not True:
+                fact_checks[f] = check_efarsas(source, sleep_min, sleep_max)
+                continue
+
+            f = 'g1.globo.com/e-ou-nao-e'  # É ou não é?
+            if f in source and fact_checks[f] is not True:
+                fact_checks[f] = check_e_ou_nao_e(source, sleep_min, sleep_max)
+                continue
+
+            f = 'piaui.folha.uol.com.br/lupa'  # Lupa
+            if f in source and fact_checks[f] is not True:
+                fact_checks[f] = check_lupa(source, sleep_min, sleep_max)
+                continue
+
+    return fact_checks
